1c955ed30f0b33eb135044b8d5ce3c8e52b86fde
Jason Myers <j.myers@brstrat.com>
2014-11-13 15:30:28 -0500

    Patch wtforms validate_on_submit to log (debug) form errors

--- pybb3/patches.py
+++ pybb3/patches.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import functools
+
+import logging
+logger = logging.getLogger(__name__)
+
 
 def debugger():
     """ Adds a 'debugger' command to builtins """
@@ -19,3 +24,19 @@ def debugger():
     pdb.set_trace(trace_frame)
 
 __builtins__['debugger'] = debugger
+
+
+from flask_wtf.form import Form
+original_validate_on_submit = Form.validate_on_submit
+
+
+@functools.wraps(original_validate_on_submit)
+def log_form_errors(self, *args, **kwargs):
+    try:
+        return original_validate_on_submit(self, *args, **kwargs)
+    finally:
+        if self.errors:
+            logger.debug("Errors in {form}: {errors}".format(
+                form=self, errors=self.errors))
+
+Form.validate_on_submit = log_form_errors
